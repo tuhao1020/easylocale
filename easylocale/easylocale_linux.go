6e2fe45da4c9afbf8ff12dd8c218667658ce454b
tuhao1020 <tuhao1020@qq.com>
2021-07-07 19:14:57 +0800

    add linux support

---
+++ easylocale/easylocale_linux.go
@@ -0,0 +1,42 @@
+package easylocale
+
+import (
+	"fmt"
+	"golang.org/x/sys/windows"
+	"unsafe"
+)
+
+const (
+	nameMaxLength = 85
+	nullptr = 0
+	dllName = "kernel32"
+)
+
+var procNames = []string{"GetUserDefaultLocaleName", "GetSystemDefaultLocaleName"}
+
+func getWindowsLocaleFromProc(procName string) (string, error) {
+	dll := windows.MustLoadDLL(dllName)
+	proc := dll.MustFindProc(procName)
+
+	buffer := make([]uint16, nameMaxLength)
+	r, _, err := proc.Call(uintptr(unsafe.Pointer(&buffer[0])), uintptr(nameMaxLength))
+	if r == nullptr {
+		return "", fmt.Errorf("invoke %s: %v", procName, err)
+	}
+
+	return windows.UTF16ToString(buffer), nil
+}
+
+func CurrentLocale() (string, error) {
+	var locale string
+	var err    error
+
+	for _, proc := range procNames {
+		locale, err = getWindowsLocaleFromProc(proc)
+		if err != nil {
+			continue
+		}
+	}
+
+	return locale, err
+}
